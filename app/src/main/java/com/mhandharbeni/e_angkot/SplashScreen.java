f83d5319aa63734248d284212318be07b1772b88
mhandharbeni <mhandharbeni@gmail.com>
2019-08-02 16:28:48 +0700

    update splash screen update basecolor

--- app/src/main/java/com/mhandharbeni/e_angkot/SplashScreen.java
+++ app/src/main/java/com/mhandharbeni/e_angkot/SplashScreen.java
@@ -1,8 +1,10 @@
 package com.mhandharbeni.e_angkot;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
 import com.mhandharbeni.e_angkot.main_activity.LoginActivity;
@@ -10,14 +12,27 @@ import com.mhandharbeni.e_angkot.second_activity.user.MainActivity;
 import com.mhandharbeni.e_angkot.utils.BaseActivity;
 import com.mhandharbeni.e_angkot.utils.Constant;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SplashScreen extends BaseActivity {
+    @BindView(R.id.appnameversion)
+    TextView appnameversion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         ButterKnife.bind(this);
+
+        try {
+            String versionName = getApplicationContext().getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0).versionName;
+            String appName = "V";
+            appnameversion.setText(String.format("%s %s", appName, versionName));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
         hideActionBar();
         new Handler().postDelayed(() -> {
             if (getPref(Constant.IS_LOGGIN, false)) {
